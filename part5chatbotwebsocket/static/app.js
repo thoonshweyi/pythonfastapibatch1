49623876674c5d0bdf50ef49de96922762708550
Thoon Shwe Yi Kyaw <thoonlay779@gmail.com>
2026-07-15 22:53:41 +0630

    15JUL

---
+++ part5chatbotwebsocket/static/app.js
@@ -0,0 +1,48 @@
+// UI
+
+console.log("hello");
+var ws = new WebSocket("ws://localhost:8000/ws");
+
+const sendbtn = document.getElementById('send-btn');
+const userinput = document.getElementById('userinput');
+const displaybox = document.getElementById('displaybox');
+
+var ws = new WebSocket("ws://localhost:8000/ws");
+
+ws.onopen = function(event) {
+     console.log("WebSocket connection established.");
+}
+
+ws.onerror = function(err) {
+     console.log("WebSocket connection error: ",error);
+     document.getElementById('loading-spinner').style.display = "none";
+}
+
+ws.onerror = function(event) {
+     console.log("WebSocket connection closed: ",event);
+     document.getElementById('loading-spinner').style.display = "none";
+}
+
+ws.onmessage = function(event){
+
+}
+
+
+sendbtn.addEventListener('click',function(e){
+     e.preventDefault();
+
+     let getinputval = userinput.value.trim();
+
+     if(getinputval){
+          var userinputdiv = document.createElement("div");
+          userinputdiv.className = "p-3 ms-3 chat-message user-input";
+          userinputdiv.textContent = getinputval;
+          displaybox.appendChild(userinputdiv);
+
+          ws.send(getinputval); // to websocket
+
+          userinput.value = "";
+          userinput.focus();
+
+     }
+});
